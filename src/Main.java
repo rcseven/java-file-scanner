cfa67704fca4db304cdcc55ce998c4622b8a3bda
rcseven <rcedquila@gmail.com>
2023-04-20 14:33:18 +0800

    Initial Commit

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,17 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) throws Exception{
-        File file = new File("sample.txt");
-        Scanner filename = new Scanner(file);
+    public static void main(String[] args) {
+        try {
+            File file = new File("sample.txt");
+            Scanner filename = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found...");
+        }
 
         int countwords = 0;
         while(filename.hasNextLine()) {
